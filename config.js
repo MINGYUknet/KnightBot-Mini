b034154f399937eec241ad76051758e10a5c6bde
OneDev <107590883+MINGYUknet@users.noreply.github.com>
2026-03-30 17:12:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,11 +4,11 @@
 
 module.exports = {
     // Bot Owner Configuration
-    ownerNumber: ['91xxxxxxxxxxx','917023951514'], // Add your number without + or spaces (e.g., 919876543210)
-    ownerName: ['Knight Bot Mini', 'Professor'], // Owner names corresponding to ownerNumber array
+    ownerNumber: ['6285363155850'], // Add your number without + or spaces (e.g., 919876543210)
+    ownerName: ['ShiniGami'], // Owner names corresponding to ownerNumber array
     
     // Bot Configuration
-    botName: 'Knight Bot Mini',
+    botName: 'ShiniGami',
     prefix: '.',
     sessionName: 'session',
     sessionID: process.env.SESSION_ID || '',
@@ -16,7 +16,7 @@ module.exports = {
     updateZipUrl: 'https://github.com/mruniquehacker/KnightBot-Mini/archive/refs/heads/main.zip', // URL to latest code zip for .update command
     
     // Sticker Configuration
-    packname: 'Knight Bot Mini',
+    packname: 'ShiniGami',
     
     // Bot Behavior
     selfMode: false, // Private mode - only owner can use commands
